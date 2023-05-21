fa0e341f8c4625d7a7eb75bb18a1a658d49524eb
LeeBhin <bini4039@naver.com>
2023-05-21 21:03:29 +0900

    School List Function Exception

--- src/functions/Sch_List.js
+++ src/functions/Sch_List.js
@@ -11,7 +11,7 @@ function SchList(School) {
     return Only_Nm.join(', ')
 }
 
-function Need_Info() {
+function Need_Info() {  //필요한 정보만
     var result = [];
     for (const item of All_Info) {
         const schul_code = item.SCHUL_CODE;
@@ -25,7 +25,7 @@ function Need_Info() {
             "HS_KND_SC_NM": hs_knd_sc_nm    //종류
         });
     }
-    console.log('needinfo:', result)
+
     return result;
 }
 
@@ -40,6 +40,7 @@ function findMatchingItems(jsonData, keywords) {
         const rdnma = data.SCHUL_RDNMA;
         let matchCount = 0;
 
+        //예외 처리
         if (keywordsToCheck.includes('경남') && rdnma.includes('경상남도')) {
             matchCount += 2;
         }
@@ -59,39 +60,42 @@ function findMatchingItems(jsonData, keywords) {
             matchCount += 2;
         }
 
-        for (let i = 0; i < keywordsToCheck.length; i++) {
-            const keyword = keywordsToCheck[i];
-
-            if (i === 0) {  //첫번째는 단어로
-                if (rdnma.includes(keyword)) {
+        for (const keyword of keywordsToCheck) {
+            let prevChar = ''; // 이전 글자
+            for (const char of keyword) {
+                if (prevChar && rdnma.includes(prevChar + char)) {
+                    // 이전 글자와 현재 글자를 합친 문자열이 주소에 포함되어 있는 경우
                     matchCount += 2;
-
                 }
-
-            } else {    //나머지는 글자로
-                for (const char of keyword) {
-                    if (rdnma.includes(char)) {
-                        matchCount++;
-                    }
-                }
+                prevChar = char; // 현재 글자를 이전 글자로 저장
             }
         }
-        if (matchCount >= count) {
+
+        if (matchCount >= count) {  //이전 조건이 충족하는경우
             let lastWordMatchCount = 0;
             for (const char of lastWord) {
-                if (data.HS_KND_SC_NM.includes(char)) {
+                if (data.HS_KND_SC_NM.includes(char)) { //한글자씩 검사
                     lastWordMatchCount++;
                 }
             }
 
-            if (lastWordMatchCount >= 3) {
+            if (lastWordMatchCount >= 3) {  //3글자 이상일떄
                 matchedData.push(data);
             }
         }
     }
 
-    return matchedData;
-}
+    const filteredData = [];
 
+    for (const data of matchedData) {
+        const firstTwoChars = keywordsToCheck[0].substring(0, 2); // 문자열 리스트의 첫 번째 요소의 첫 두글자
+
+        if (data.SCHUL_RDNMA.includes(firstTwoChars)) {
+            filteredData.push(data);
+        }
+    }
+
+    return filteredData;
+}
 
 export { SchList }
