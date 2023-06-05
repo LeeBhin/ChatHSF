740bd655b00e130a9879ab5c2ab7a00fbff2f9d7
LeeBhin <bini4039@naver.com>
2023-06-05 17:25:26 +0900

    Update api.js
    Current Year api

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,11 @@ const https = require('https');
 
 exports.handler = async function (event, context, callback) {
   try {
+    const currentYear = new Date().getFullYear().toString();
+
     const urls = [
-      'https://www.schoolinfo.go.kr/openApi.do?apiKey=09f9b2bf2e3b4daea8e4f4b88f22b7df&apiType=0&pbanYr=2023&schulKndCode=04',
-      'https://www.schoolinfo.go.kr/openApi.do?apiKey=09f9b2bf2e3b4daea8e4f4b88f22b7df&apiType=63&pbanYr=2022&schulKndCode=04'
+      `https://www.schoolinfo.go.kr/openApi.do?apiKey=09f9b2bf2e3b4daea8e4f4b88f22b7df&apiType=0&pbanYr=${currentYear}&schulKndCode=04`,
+      `https://www.schoolinfo.go.kr/openApi.do?apiKey=09f9b2bf2e3b4daea8e4f4b88f22b7df&apiType=63&pbanYr=${currentYear}&schulKndCode=04`
     ];
 
     const responses = await Promise.all(
@@ -14,7 +16,19 @@ exports.handler = async function (event, context, callback) {
           const response = await axios.get(url, {
             httpsAgent: new https.Agent({ rejectUnauthorized: false })
           });
-          return response.data;
+
+          if (response.data.resultCode === 'fail') {
+            // 작년 연도로 업데이트
+            const lastYear = (currentYear - 1).toString();
+            const updatedUrl = url.replace(currentYear, lastYear);
+
+            const updatedResponse = await axios.get(updatedUrl, {
+              httpsAgent: new https.Agent({ rejectUnauthorized: false })
+            });
+            return updatedResponse.data;
+          } else {
+            return response.data;
+          }
         } catch (error) {
           console.error('Failed to fetch data:', error);
           throw error;
